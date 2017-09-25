0f72a8b665297c4a310838a0f51f2c6d32d94e18
jack6785123 <106021154@live.asia.edu.tw>
2017-09-25 15:48:38 +0800

    first commit

---
+++ src/homework2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class homework2{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int gram = scn.nextInt();
+        System.out.println(Math.round(gram/600f));
+    }
+}
+
